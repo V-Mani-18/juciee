4dda4a9caa88f8e7799bbc88541ff8e7c3856242
Manikandan <bpeace450@gmail.com>
2025-07-17 12:09:44 +0530

    Update User.js

--- User.js
+++ User.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: true }, // Added
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
   gender: { type: String, required: true },
-  govidproof: { type: String }, // Added (store file path or URL)
-  profileImage: { type: String ,default: ''}, // base64 string
+  govidproof: { type: String },
+  profileImage: { type: String ,default: ''},
+  friends: [{
+    friendId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    username: { type: String }, // Store friend's username for fast lookup
+    profilePic: { type: String, default: '' }
+  }]
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
